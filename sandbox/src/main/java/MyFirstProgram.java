a4f39c7858bc90170e81ebb701633bbf6bb04294
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-12-02 19:57:28 +0300

    Начальный

---
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -0,0 +1,13 @@
+public class MyFirstProgram {
+  static String name="Ruslan";
+
+  public static void main(String[] args) {
+    helloRadik();
+    helloRadik();
+    helloRadik();
+  }
+  public static void helloRadik() {
+    System.out.println("Hello, "+name);
+  }
+
+}
